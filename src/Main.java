09db1ef1e507a8dedcd8809254ecc636bcbb6ee7
AbduSomad <abdusomadkoshmamatov@gmail.com>
2023-03-15 08:25:25 +0600

    f

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,18 @@ public class Main {
 //Todo бул жерде 3 массив бар. Бири рандомдон толтурат экинчиси жуп сан, учунчусу так сандарды чыгарат.
         Random random = new Random();
         List<Integer> num = new ArrayList<>();
+        List<Integer> sum1 = new ArrayList<>(); //Жуп сан чыгарган массив.
+        List<Integer> sum2 = new ArrayList<>(); // Так сан.
         for (int i = 0; i < 100; i++) {
             num.add(random.nextInt(0, 50));
-            System.out.println(num.get(i));
+            System.out.print(num.get(i)+" ");
             if (num.get(i) % 2 == 0) {
-                List<Integer> sum1 = new ArrayList<>(List.of(num.get(i))); //Жуп сан чыгарган массив.
-                System.out.println(sum1);
+                sum1.add(num.get(i));
             } else {
-                List<Integer> sum2 = new ArrayList<>(List.of(num.get(i))); // Так сан.
-                System.out.println(sum2);
+                sum2.add(num.get(i));
             }
-
         }
+        System.out.println("\nЖуп сандар: "+sum1);
+        System.out.println("Так сандар: "+sum2);
     }
 }
